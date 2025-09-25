ee51c8f1900adb0fa0f0a17c9478a8f32da57dd3
Shubham Giri <shubhammgiri3@gmail.com>
2025-09-25 22:02:22 +0530

    impliment game loop

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+
 def main():
-    print("Hello from asteroid-game!")
+    pygame.init()
+    print("Starting Asteroids!")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
